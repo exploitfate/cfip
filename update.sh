d8f8fee0bbeb6e7b5d9524318a8953b9cded9a23
casper <casper@inernetbs.net>
2025-05-13 13:06:19 +0300

    Add custom ip address for revers proxy support

--- update.sh
+++ update.sh
@@ -7,6 +7,9 @@ CF_NGINX_CONFIG="/etc/nginx/conf.d/cloudflare.conf"
 CF_URL_IP4="https://www.cloudflare.com/ips-v4/"
 CF_URL_IP6="https://www.cloudflare.com/ips-v6/"
 
+# Custom IP file
+CUSTOM_IP="$(dirname $0)/custom.txt"
+
 # Temporary files.
 CF_TEMP_IP4="/tmp/cloudflare-ips-v4.txt"
 CF_TEMP_IP6="/tmp/cloudflare-ips-v6.txt"
@@ -34,8 +37,17 @@ MAX_LINES=20
 if [[ "$CF_TEMP_IP4_LINES" -le "$MAX_LINES" ]] && [[ "$CF_TEMP_IP6_LINES" -le "$MAX_LINES" ]]; then
 
   # Generate the new config file.
-  echo "# CloudFlare IP Ranges" > $CF_NGINX_CONFIG
-  echo "# Generated at $(date) by $0" >> $CF_NGINX_CONFIG
+  echo "# Generated at $(date) by $0" > $CF_NGINX_CONFIG
+  echo "# Custom IP Ranges" >> $CF_NGINX_CONFIG
+
+  echo "" >> $CF_NGINX_CONFIG
+
+  awk '{ print "set_real_ip_from " $0 ";" }' $CUSTOM_IP >> $CF_NGINX_CONFIG
+
+  echo "" >> $CF_NGINX_CONFIG
+
+
+  echo "# CloudFlare IP Ranges" >> $CF_NGINX_CONFIG
 
   echo "" >> $CF_NGINX_CONFIG
 
